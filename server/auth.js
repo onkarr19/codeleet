8c463ebe2f273d10f0f6570886fec2eb575a3aef
onkar <onkar.chendage@gmail.com>
2023-08-09 12:58:47 +0530

    add comments

--- server/auth.js
+++ server/auth.js
@@ -4,14 +4,32 @@ dotenv.config();
 
 const JWT_SECRET = process.env.JWT_SECRET;
 
+// custom middleware function for authentication
 export const auth = (req, res, next) => {
+    
+    // get the 'Authorization' header from the request headers
     const authHeader = req.headers['authorization'];
-    if (!authHeader) return res.status(403).json({message: "Missing Auth Header"});
+    if (!authHeader) {
+        // ff the 'Authorization' header is missing, return a 403 (Forbidden) response
+        return res.status(403).json({ message: "Missing Auth Header" });
+    }
 
-    const decoded = jwt.verify(authHeader, JWT_SECRET);
-    if (decoded && decoded.id) {
+    try {
+        // verify the JWT token using the JWT_SECRET
+        const decoded = jwt.verify(authHeader, JWT_SECRET);
+
+        if (decoded && decoded.id) {
+        // if the token is valid and contains a user ID, store the user ID in the request object for later use
         req.userId = decoded.id;
+
+        // proceed to the next middleware or route handler
         next();
-    } 
-    else return res.status(403).json({message: "Incorrect Token"});
+        } else {
+        // If the token is invalid or doesn't contain a user ID, return a 403 (Forbidden) response
+        return res.status(403).json({ message: "Incorrect Token" });
+        }
+    } catch (err) {
+        // If there's an error while verifying the token, return a 403 (Forbidden) response
+        return res.status(403).json({ message: "Invalid Token" });
+    }
 };
--- server/index.js
+++ server/index.js
@@ -1,5 +1,5 @@
 import express from 'express';
-import {auth} from './auth.js';
+import { auth } from './auth.js';
 import jwt from 'jsonwebtoken';
 import cors from 'cors';
 
@@ -18,8 +18,8 @@ app.use(cors({
 
 // Dummy data
 const PROBLEMS = [
-    {   
-        id: 1, 
+    {
+        id: 1,
         title: "Two Sum",
         description: "Given an array of integers nums and an integer target, return indices of the two numbers such that they add up to target.",
         difficulty: "Easy",
@@ -87,54 +87,84 @@ app.get('/', (req, res) => {
 });
 
 app.post('/signup', (req, res) => {
+    
+    // extract the username and password from the request body
     const username = req.body.username;
     const password = req.body.password;
+    const email = req.body.email;
 
+    // check if the username already exists
     if (USERS.find(x => x.username === username)) {
+        // if exists, return error message
         return res.status(411).json({
             message: "Username already exists!",
         });
     }
-    USERS.push({id: ++USER_ID_COUNTER, username: username, password: password});
-    return res.json({message: "success", USERS});
+
+    // create a new user and add save it
+    USERS.push({ id: ++USER_ID_COUNTER, username: username, password: password, email: email });
+
+    // return a JSON response indicating successful signup
+    return res.json({ message: "success", USERS });
 });
 
 app.post('/login', (req, res) => {
 
+    // extract the username and password from the request body
     const username = req.body.username;
     const password = req.body.password;
 
+    // check if user exists
     const user = USERS.find(x => x.username === username);
 
-    if (!user) 
-        return res.status(411).json({message: "User not found."});
-    if (user.password !== password) 
-        return res.status(411).json({message: "Incorrect Password"});
+    // if not not exist, return error message
+    if (!user)
+        return res.status(411).json({ message: "User not found." });
 
+    // if password does not exist, return error message
+    if (user.password !== password)
+        return res.status(411).json({ message: "Incorrect Password" });
+
+    // create a JWT token with the user's ID as the payload and sign it with the JWT secret key
     const token = jwt.sign({
         id: user.id,
     }, JWT_SECRET);
 
-    return res.json({message: "Login Success", token: token});
+    // return a JSON response indicating successful login along with the JWT token
+    return res.json({ message: "Login Success", token: token });
 });
 
 app.get('/profile', auth, (req, res) => {
-    const user = USERS.find((x) => x.id === req.userId); 
+    
+    // retrieve the user from the USERS array using the `req.userId` set by the 'auth' middleware
+    const user = USERS.find((x) => x.id === req.userId);
+    // if the user does not exist, return a 404 Not Found response
+    if (!user) {
+        return res.status(404).json({ message: 'User not found' });
+    }
+    
+    // return a JSON response containing the user's profile information
     res.json({
-        user
+        'username': user.username,
+        'name': user.name,
+        'email': user.email
     });
 });
 
 app.get('/problem/:id', (req, res) => {
+
+    // return the problem with the given id if it exists else return a 411 Not Found response 
     const id = req.params.id;
     const problem = PROBLEMS.find(problem => problem.id == id);
     if (problem) {
-        res.json({problem,});
+        res.json({ problem, });
     }
-    return res.status(411).json({error: "Problem not found."});
+    return res.status(411).json({ error: "Problem not found." });
 });
 
 app.get('/problem', (req, res) => {
+
+    // return all problems
     const filteredProblems = PROBLEMS.map((problem) => ({
         problemId: problem.id,
         difficulty: problem.difficulty,
