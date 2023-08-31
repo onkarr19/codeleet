af4fe0be06179d8bd817d36d28011e37d687fbab
onkar <onkar.chendage@gmail.com>
2023-08-31 12:00:53 +0530

    add navbar

---
+++ client/src/Constants/Navbar/Navbar.jsx
@@ -0,0 +1,62 @@
+import React, { useState, useEffect } from 'react';
+import { Link } from 'react-router-dom';
+
+import config from '../../../config';
+const backendURL = config.backendUrl;
+
+import './Navbar.css';
+
+const Navbar = () => {
+	const [isLoggedIn, setIsLoggedIn] = useState(false);
+
+	useEffect(() => {
+		const checkAuthentication = async () => {
+			const token = `${localStorage.getItem('token')}`;
+			if (token) {
+				try {
+					const response = await fetch(`${backendURL}/profile`, {
+						method: 'GET',
+						headers: {
+							Authorization: token,
+							'Content-Type': 'application/json'
+						},
+					});
+
+					if (response.ok) {
+						setIsLoggedIn(true);
+					}
+				} catch (error) {
+					console.error('Error checking authentication:', error);
+				}
+			}
+		};
+
+		checkAuthentication();
+	}, []);
+
+	return (
+		<div id='navbar-main' className='flex-row'>
+			<Link to={'/'}>
+				<div className="logo-box flex-row">
+					<img className='logo' src="https://user-images.githubusercontent.com/63964149/152531278-5e01909d-0c2e-412a-8acc-4a06863c244d.png" alt="logo" />
+					<p>CodeLeet</p>
+				</div>
+			</Link>
+			<div className="nav-options">
+				<Link to={'/problemset'} >Problems</Link>
+			</div>
+
+			{isLoggedIn ? (
+				<div className="nav-options">
+					<Link to={'/profile'}>Profile</Link>
+				</div>
+			) : (
+				<div className="nav-options">
+					<Link to={'/login'}>Login</Link>
+				</div>
+			)}
+		</div>
+	);
+}
+
+export default Navbar;
