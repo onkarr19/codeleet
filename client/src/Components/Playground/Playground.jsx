a559fca2c3fa3fedd534e05aad9b165d71659e02
onkar <onkar.chendage@gmail.com>
2023-09-02 23:01:11 +0530

    feature: multi-lang code editor

--- client/src/Components/Playground/Playground.jsx
+++ client/src/Components/Playground/Playground.jsx
@@ -1,14 +1,54 @@
-import React from 'react';
+import React, { useState } from 'react';
+import Editor from '@monaco-editor/react';
 
-function App({ value, onChange }) {
+import './Playground.css'
+
+function App() {
+    const [language, setLanguage] = useState('javascript');
+    const [code, setCode] = useState({
+        javascript: '// JavaScript code here',
+        python: '# Python code here',
+        cpp: '// C++ code here',
+    });
+
+    const handleLanguageChange = (newLanguage) => {
+        setLanguage(newLanguage);
+    };
+
+    const handleEditorChange = (newCode) => {
+        setCode((prevCode) => ({
+            ...prevCode,
+            [language]: newCode,
+        }));
+    };
+
+    const getCode = () => {
+        console.log(code[language])
+    }
+
     return (
         <div>
-            <input
-                type="text"
-                value={value}
-                onChange={onChange}
-            // Add any other props or styles you need
-            />
+            <div>
+                <select
+                    id="languageDropdown"
+                    value={language}
+                    onChange={(e) => handleLanguageChange(e.target.value)}
+                >
+                    <option value="javascript">JavaScript</option>
+                    <option value="python">Python</option>
+                    <option value="cpp">C++</option>
+                </select><button onClick={() => getCode()}>Run</button>
+            </div>
+            <div className="overlay rounded-md overflow-hidden w-full h-full shadow-4xl">
+                <Editor
+                    height="80vh"
+                    width="100%"
+                    language={language}
+                    value={code[language]}
+                    theme="vs-dark"
+                    onChange={handleEditorChange}
+                />
+            </div>
         </div>
     );
 }
