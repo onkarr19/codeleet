aa0316732e4d525ed6ceed6bed95c8e4affed2d0
onkar <onkar.chendage@gmail.com>
2023-09-02 13:36:15 +0530

    update problemset

--- client/src/Components/Problemset/Problemset.jsx
+++ client/src/Components/Problemset/Problemset.jsx
@@ -1,8 +1,50 @@
-import React from "react";
+import React, { useState, useEffect } from "react";
+
+import config from '../../../config';
+import { Link } from "react-router-dom";
+const backendURL = config.backendUrl;
 
 function App() {
+
+    const [problems, setProblems] = useState(null);
+    useEffect(() => {
+        const fetchProblems = async () => {
+            try {
+                const response = await fetch(`${backendURL}/problem`, {
+                    method: "GET",
+                    headers: {
+                        'Content-Type': 'application/json'
+                    },
+                });
+                if (!response.ok) {
+                    throw new Error(`HTTP error! Status: ${response.status}`);
+                }
+                const data = await response.json();
+                setProblems(data.problems);
+            } catch (error) {
+                console.error("Error fetching data:", error);
+            }
+        };
+
+        fetchProblems();
+    }, []);
     return <>
         <h2>Problems Page</h2>
+        { problems ? (
+                <ul>
+                    {problems.map((problem) => (
+                        <li key={problem.problemId}>
+                            <strong>Title:</strong> <Link to= {`/problem/${problem.problemId}`} > {problem.title} </Link> <br />
+                            <strong>Difficulty:</strong> {problem.difficulty}<br />
+                            <strong>Acceptance:</strong> {problem.acceptance}<br />
+                            <br></br>
+                            </li>
+                    ))}
+                </ul>
+            ) : (
+                <p>Loading problems...</p>
+            )}
+        
     </>
 }
 
