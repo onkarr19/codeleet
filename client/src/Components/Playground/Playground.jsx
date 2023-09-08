57d960c816794036dc0f7c6e4f2816adc90451ea
onkar <onkar.chendage@gmail.com>
2023-09-08 18:08:17 +0530

    add: run button

--- client/src/Components/Playground/Playground.jsx
+++ client/src/Components/Playground/Playground.jsx
@@ -1,7 +1,10 @@
 import React, { useState } from 'react';
 import Editor from '@monaco-editor/react';
 
-import './Playground.css'
+import './Playground.css';
+
+import config from '../../../config';
+const backendURL = config.backendUrl;
 
 function App() {
     const [language, setLanguage] = useState('javascript');
@@ -10,6 +13,8 @@ function App() {
         python: '# Python code here',
         cpp: '// C++ code here',
     });
+    const [error, setError] = useState();
+    const [output, setOutput] = useState();
 
     const handleLanguageChange = (newLanguage) => {
         setLanguage(newLanguage);
@@ -22,8 +27,39 @@ function App() {
         }));
     };
 
-    const getCode = () => {
-        console.log(code[language])
+    const handleRunCode = async (e) => {
+        try {
+            const response = await fetch(`${backendURL}/submission`, {
+                method: 'POST',
+                headers: {
+                    'Content-Type': 'application/json',
+                    'Authorization': localStorage.getItem('token')
+                    // Add any other headers you might need, such as authentication tokens
+                },
+                body: JSON.stringify({
+                    lang: language,
+                    code: code[language],
+                    problem_id: 2,
+                    submit: false,
+                }),
+            });
+
+            if (response.ok) {
+                const data = await response.json();
+                console.log(data);
+                setOutput(data.message);
+                setError();
+            } else {
+                // Handle the error response here
+                setError('Failed to submit');
+                setOutput();
+                console.error('Failed to submit');
+            }
+        } catch (error) {
+            // console.error('An error occurred:', error);
+            setError('Failed to submit');
+            setOutput();
+        }
     }
 
     return (
@@ -37,7 +73,10 @@ function App() {
                     <option value="javascript">JavaScript</option>
                     <option value="python">Python</option>
                     <option value="cpp">C++</option>
-                </select><button onClick={() => getCode()}>Run</button>
+                </select><button onClick={() => handleRunCode()}>Run</button>
+                
+                <div>{error && <p>error: {error}</p>}</div>
+                <div>{output && <p>output: {output}</p>}</div>
             </div>
             <div className="overlay rounded-md overflow-hidden w-full h-full shadow-4xl">
                 <Editor
